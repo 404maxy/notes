c0f08a654f5a7368302926e13f4e27128965c334
maxy404 <404maxy@gmail.com>
2024-07-12 00:12:20 +0300

    Конфигурация

--- www/notes/common/config/main.php
+++ www/notes/common/config/main.php
@@ -6,6 +6,14 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+//       TODO: предоставить настройки для подключения к базе в main-local.php в сопроводительном письме
+//        'db' => [
+//            'class' => \yii\db\Connection::class,
+//            'dsn' => 'mysql:host=notes_mysql;dbname=notes',
+//            'username' => 'notes',
+//            'password' => 'n0t3sPwd',
+//            'charset' => 'utf8',
+//        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
